88bbdfd7bd063901b04370cd1c27865073591630
Helen85236 <79532870251@yandex.ru>
2023-09-12 22:45:46 +0300

    List of lessons to course serializer

--- courses/serializers.py
+++ courses/serializers.py
@@ -5,13 +5,15 @@ from courses.models import Lesson, Course
 
 
 class LessonSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Lesson
         fields = '__all__'
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    lesson_count = IntegerField(source='lesson_set.count')
+    lesson_count = IntegerField(source='lesson_set.count', required=False)
+    lessons = LessonSerializer(many=True)
 
     class Meta:
         model = Course
