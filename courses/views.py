b16867250a91a91e63cd3288ca10aae1becbe0ed
Helen85236 <79532870251@yandex.ru>
2023-09-15 21:34:59 +0300

    Add restriction

--- courses/views.py
+++ courses/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, viewsets
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import IsAuthenticated
 
 from courses.models import Lesson, Course, Payment
 from courses.serializers import CourseSerializer, LessonSerializer, \
@@ -11,29 +12,41 @@ from courses.serializers import CourseSerializer, LessonSerializer, \
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+    # Define permissions
+    permission_classes = [IsAuthenticated]
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    # Define permissions
+    permission_classes = [IsAuthenticated]
 
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    # Define permissions
+    permission_classes = [IsAuthenticated]
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    # Define permissions
+    permission_classes = [IsAuthenticated]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    # Define permissions
+    permission_classes = [IsAuthenticated]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
+    # Define permissions
+    permission_classes = [IsAuthenticated]
 
 
 class PaymentListAPIView(generics.ListAPIView):
@@ -42,3 +55,5 @@ class PaymentListAPIView(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     ordering_fields = ('date_paid',)
     filterset_fields = ('course', 'lesson', 'type',)
+    # Define permissions
+    permission_classes = [IsAuthenticated]
