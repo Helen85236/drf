f4cf8d4d32516c670be6b150516b1593ffc3bcfc
Helen85236 <79532870251@yandex.ru>
2023-09-28 20:26:55 +0300

    Installing celery and celery-beat

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,11 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'django_filters',
+    # For commenting
+    'django.contrib.admindocs',
+
+    # Celery
+    'django_celery_beat',
 
     'users',
     'courses',
@@ -149,3 +154,7 @@ REST_FRAMEWORK = {
 }
 
 STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
+
+# Celery settings
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
